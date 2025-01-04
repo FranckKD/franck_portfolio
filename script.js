db981505808c048a623e7f63239e1107d52c3af6
FranckKD <djidjifranck2000@gmail.com>
2025-01-04 20:19:27 +0000

    update

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-// Fonction pour activer/désactiver le menu hamburger sur les petits écrans
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector("#menu-toggle"); // Le bouton hamburger
-    const navbar = document.querySelector(".navbar ul"); // Liste des éléments du menu
-
-    // Ajout d'un événement pour basculer le menu
-    menuToggle.addEventListener("click", function() {
-        navbar.classList.toggle("active"); // Ajouter ou retirer la classe 'active' pour afficher/cacher le menu
-    });
-
-    // Optionnel : fermer le menu lorsque l'un des liens est cliqué
-    const menuLinks = document.querySelectorAll(".navbar ul li a");
-    menuLinks.forEach(link => {
-        link.addEventListener("click", function() {
-            navbar.classList.remove("active"); // Fermer le menu après un clic sur un lien
-        });
-    });
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  if (link.href.includes(activePage)) {
+    link.classList.add('active');
+    console.log(link);
+  }
 });
 
-document.querySelectorAll('.toggle-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        const details = button.parentElement.nextElementSibling;
-        // Toggle l'affichage des détails sans changer le texte du bouton
-        if (details.style.display === "none" || details.style.display === "") {
-            details.style.display = "block";
-        } else {
-            details.style.display = "none";
-        }
-    });
-});
-
-const menuIcon = document.querySelector('.menu-icon');
-const navbar = document.querySelector('.navbar ul');
-
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
-
+  
+const menuHamburger = document.querySelector(".menu-hamburger")
+        const menuLinks = document.querySelector(".nav-links")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        menuLinks.classList.toggle('mobile-menu')
+        })
